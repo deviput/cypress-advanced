a83c1745f63c2ddb3e5b47681539136c468c76be
deviput <devifajar2424@gmail.com>
2023-01-21 10:31:11 +0700

    push cypress

---
+++ cypress/e2e/login.test.cy.js
@@ -0,0 +1,40 @@
+/// reference types="cypress" />
+
+describe('Login/ Logout Test', () => {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').click()
+    });
+
+    it('should try to login with invalid data', () => {
+        cy.get('#login_form').should('be.visible')
+        cy.get('#user_login').type('invalid username')
+        cy.get('#user_password').type('invalid password')
+        cy.get('input[name="submit"]').click()
+    });
+
+    it('should display error message', () => {
+        cy.get('.alert-error').should('be.visible').and('contain', 'Login and/or password are wrong.')
+    });
+
+    it('should login to application with valid data', () => {
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type('username')
+            cy.get('#user_password').clear()
+            cy.get('#user_password').type('password')
+            cy.get('input[name="submit"]').click()
+            cy.get('a').should('contain.text', 'Zero Bank')
+        })
+    });
+
+    it('should logout from apllication', () => {
+        cy.contains('username').click()
+        cy.get('#logout_link').click()
+        cy.get('.strong').should('contain.text', 'Home')
+    })
+});
---
+++ cypress/e2e/searchbox.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+
+describe('Searchbox Test', () => {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    });
+
+    it('Should type into searchbox and submit', () => {
+        cy.get('#searchTerm').type('some text {enter}')
+        cy.get('h2').should('contain.text', 'Search Results:')
+
+    });
+});
